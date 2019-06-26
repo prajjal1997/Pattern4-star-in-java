4c83bc2130fd5f9b343dbce241000dd68c474aca
prajjal saxena <52135244+prajjal1997@users.noreply.github.com>
2019-06-26 11:58:47 -0700

    Update Pattern4.java

--- Pattern4.java
+++ Pattern4.java
@@ -5,11 +5,11 @@ public class Pattern4 {
 		// TODO Auto-generated method stub
 		int i,j;
 		for(i=1;i<=5;i++)                                           
-		{                                                  //             
-			for(j=1;j<=9;j++) {                             //   
-			if(j>=6-i&&j<=4+i)                                       //    
-				System.out.print("*");                     //       
-			else                                          //         
+		{                                                  //                  *  
+			for(j=1;j<=9;j++) {                             //            ***
+			if(j>=6-i&&j<=4+i)                              //           *****   
+				System.out.print("*");                     //       *******
+			else                                          //           *********
 			System.out.print(" ");
 			}
 			System.out.println();
